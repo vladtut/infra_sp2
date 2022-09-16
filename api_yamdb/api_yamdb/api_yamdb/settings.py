3beb4b83445e954f1c342cd90a0684081f2592ae
vladvm <vobrembalskii@gmail.com>
2022-09-16 21:45:03 +0300

    проект

--- api_yamdb/api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/api_yamdb/settings.py
@@ -117,7 +117,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Authentification
 AUTH_USER_MODEL = 'reviews.User'
